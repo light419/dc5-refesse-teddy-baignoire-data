18d8eeef88b082d12feb86b873a32f03a57add0e
light419 <124808354+light419@users.noreply.github.com>
2023-11-09 16:01:17 +0100

    ajout exercice 1

---
+++ ex1-a.py
@@ -0,0 +1,11 @@
+# EXERCICE 1 - a
+# Créez des variables pour stocker le nom d'une entreprise, le nombre de clients et le revenu moyen par client. 
+# Calculez et affichez le revenu total.
+
+nom_entreprise = "Mugirawa Corp"
+nombre_clients = 107
+revenu_moyen_par_client = 57
+
+revenu_total = nombre_clients * revenu_moyen_par_client
+
+print(f"Le revenu total de {nom_entreprise} est de {revenu_total} euros.")
